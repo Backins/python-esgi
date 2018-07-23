4a6347d381c581f3c877c547bdc5c39cab30c398
Domange <backinsmail@gmail.com>
2018-07-23 17:23:14 +0200

    app.py comment

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-# Crochet permet d'utiliser des applications asynchrones Twisted dans un contexte synchrone.
+# FR - AIDE Crochet permet d'utiliser des applications asynchrones Twisted dans un contexte synchrone.
+# EN - HELP With crochet, we can use asynchrones Twisted app in synchrone context/
 
 import crochet
 crochet.setup()
@@ -7,27 +8,33 @@ from flask import Flask, render_template, request, jsonify
 from scrapy.crawler import CrawlerRunner
 from scrapper import WebSpider
 
-
 app = Flask(__name__)
 
-# On defini les parametres de notre Crawler, notamment le nombre de pages Max a crawl.
+# FR - HELP On defini les parametres de notre Crawler, notamment le nombre de pages Max a crawl.
+# EN - HELP Define settings of our crawler, number of max crawl page for example
+
 crawl_runner = CrawlerRunner({
     'CLOSESPIDER_ITEMCOUNT': 2
 })
 
-# Variable de stockage des resultats
+# FR - HELP Variable de stockage des resultats
+# EN - HELP We stock the result of our research in :param storage
+
 storage = {}
 
-# Variable de stockage des taches terminées
+# FR - HELP Variable de stockage des taches terminées
+# EN - HELP We stock completed tasks in :param scrappers_done
+
 scrappers_done = {}
 
 
 @app.route('/process', methods=['POST'])
 def process():
     """
-    Cette fonction est appelée lors de la soumission du formulaire de crawling.
-
+    FR - HELP Cette fonction est appelée lors de la soumission du formulaire de crawling.
     Elle fait appel au scrapper avec les parametres du formulaire.
+    EN - HELP This function is called when we click on submit form (on the website). process() calls Scrapp with
+    parameter of the send form
 
     """
     target_website = request.form.get('website')
@@ -39,24 +46,31 @@ def process():
 
 def process_done(target_website):
     """
-    Callback !
-
+    FR - HELP Callback !
     Cette fonction est appellée quand le scrapping d'un site est terminé.
+
+    EN - HELP Callback, we called this function when the scrapping is down
     """
 
     print('Scrapping done : %s' % target_website)
 
-    #  Un tableau indique que le crawling du site est terminé.
+    # FR - HELP Un tableau indique que le crawling du site est terminé.
+    # EN - HELP This array give us a notification when the crawling are down
+
     scrappers_done[target_website] = True
 
 
 @crochet.run_in_reactor
 def scrape_reactor(target_website, storage):
     """
-    Lance le WebSpider.
-
+    FR - HELP Lance le WebSpider.
     :param target_website: Le site à crawler.
     :param storage: Les resultats du crawler seront stockées dans cette variable.
+
+    EN - HELP Run the WebSpider
+    :param target_website : target Website to crawl
+    :param storage: the result of the crawler
+
     """
     scrapper = crawl_runner.crawl(WebSpider,
                                   target=target_website,
@@ -69,10 +83,13 @@ def scrape_reactor(target_website, storage):
 @app.route('/results', methods=['POST'])
 def results():
     """
-    Permet d'obtenir les resultats du crawler.
-
+    FR - HELP Permet d'obtenir les resultats du crawler.
     Si le crawler n'est pas terminé, alors le lien du site n'est pas disponible dans scrapper_done.
     Si le scrapper est terminé, alors on retourne l'intégralité des resultats renvoyés dans la variable storage.
+
+    EN - HELP Get the crawler result
+    If the crawler isn't finished the process, then the website link isn't give in scrapper_done
+    If the scrapper is down, then, we give the results in the storage var
     """
 
     website = request.form.get('website')
